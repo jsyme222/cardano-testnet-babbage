1aa0fd7895c0b6593b7a6abd22449d5a0fbce046
jsyme222 <jsyme222@gmail.com>
2022-09-09 11:04:39 -0600

    - Updated README. - Updated install.sh to include removeCardanoTestnet. - Removed testnet_sh directory.

--- install.sh
+++ install.sh
@@ -1,43 +1,50 @@
 #!/usr/bin/bash
 
-function installFiles () { 
+function installCardanoTestnet() {
 	dir="$HOME/.local/share/cardano-testnet-node"
 	previewDir="$dir/preview/mount"
 	preprodDir="$dir/preprod/mount"
 	binFile="$HOME/.local/bin/cardano-testnet"
 
-	if [[ ! -d $dir ]]
-	then
+	if [[ ! -d $dir ]]; then
 		mkdir $dir
 	fi
-	if [[ ! -d $previewDir ]]
-	then
+	if [[ ! -d $previewDir ]]; then
 		mkdir -p $previewDir
 	fi
-	if [[ ! -d $preprodDir ]]
-	then
+	if [[ ! -d $preprodDir ]]; then
 		mkdir -p $preprodDir
-	fi	
-	
+	fi
+
 	cp ./docker-compose-preview.yml docker-compose-preprod.yml $dir
 	echo "Installed docker-compose files at $dir"
 	cp ./preview/mount/* $previewDir
 	echo "Installed preview files at $previewDir"
 	cp ./preprod/mount/* $preprodDir
-        echo "Installed preprod files at $preprodDir"
-	
-	if [[ ! -f $binFile ]]
-	then
+	echo "Installed preprod files at $preprodDir"
+
+	if [[ ! -f $binFile ]]; then
 		touch $binFile
-		echo "#!/usr/bin/bash" >> $binFile
-		echo 'networkName=${1:-"preview"}' >> $binFile
-		echo 'containerDirection=${2-"up"}' >> $binFile
-		echo 'containerAction=$3' >> $binFile
-		echo 'testnetDir="$HOME/.local/share/cardano-testnet-node"' >> $binFile
-		echo 'docker-compose -f $testnetDir/docker-compose-$networkName.yml $containerDirection $containerAction' >> $binFile
+		echo "#!/usr/bin/bash" >>$binFile
+		echo 'networkName=${1:-"preview"}' >>$binFile
+		echo 'containerDirection=${2-"up"}' >>$binFile
+		echo 'containerAction=$3' >>$binFile
+		echo 'testnetDir="$HOME/.local/share/cardano-testnet-node"' >>$binFile
+		echo 'docker-compose -f $testnetDir/docker-compose-$networkName.yml $containerDirection $containerAction' >>$binFile
 		chmod +x $binFile
 	fi
 
 	echo "Installed Commands at $HOME/.local/bin"
 }
-installFiles
+
+function removeCardanoTestnet() {
+	echo "This will remove ALL data for both testnets. Are you sure?"
+	read -p "yes/no:  " confirm
+	echo "RESPONSE: $confirm"
+	if [[ "$confirm" != "yes" ]]; then
+		echo "Data preserved"
+	else
+		sudo rm -rf $HOME/.local/share/cardano-testnet-node/ && rm $HOME/.local/bin/cardano-testnet
+		echo "Caradano-node data removed."
+	fi
+}
